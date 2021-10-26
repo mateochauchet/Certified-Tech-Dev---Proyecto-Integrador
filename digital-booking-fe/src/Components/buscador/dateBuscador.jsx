2d869e7b70eb0afdeb01ff419d9bf60a2842ae12
Ametti12 <angiemt12@gmail.com>
2021-10-26 13:54:54 -0500

    calendario responsive

--- digital-booking-fe/src/Components/buscador/dateBuscador.jsx
+++ digital-booking-fe/src/Components/buscador/dateBuscador.jsx
@@ -1,30 +1,67 @@
-import 'react-dates/lib/css/_datepicker.css';
-import './react_dates_overrides.css'
-import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
-import 'react-dates/initialize';
+import "react-dates/lib/css/_datepicker.css";
+import "./react_dates_overrides.css";
+import {
+  DateRangePicker,
+  SingleDatePicker,
+  DayPickerRangeController,
+} from "react-dates";
+import "react-dates/initialize";
 import React, { Component } from "react";
+import {
+  HORIZONTAL_ORIENTATION,
+  VERTICAL_ORIENTATION,
+} from "react-dates/src/constants";
+import { Mobile, Default } from "./Responsive";
 
 class DateBuscador extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            startDate: null,
-            endDate: null
-         }
-    }
-    render() { 
-        return (
-            <DateRangePicker
-        startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-        endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-        focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-        onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-      />
-          );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      width: window.outerWidth,
+      orientation: VERTICAL_ORIENTATION,
+    };
+  }
+
+  render() {
+    // if(window.matchMedia("(max-width: 1200px)").matches) {
+    //     this.setState = {
+    //         orientation: HORIZONTAL_ORIENTATION
+    //     }
+    // }
+    return (
+      <>
+        <Mobile>
+          <DateRangePicker
+            numberOfMonths={1}
+            orientation={HORIZONTAL_ORIENTATION}
+            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,   
+            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+          />
+        </Mobile>
+        <Default>
+          <DateRangePicker
+            numberOfMonths={2}
+            orientation={HORIZONTAL_ORIENTATION}
+            startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+            startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+            endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+            endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+            onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+            focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,   
+            onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+          />
+        </Default>
+        
+      </>
+    );
+  }
 }
- 
+
 export default DateBuscador;
