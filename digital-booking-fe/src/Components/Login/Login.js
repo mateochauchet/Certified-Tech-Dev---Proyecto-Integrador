15439b2392ff184bb74a2bcf38f683f5f842b0dc
eversardoth <eversardoth@gmail.com>
2021-10-27 11:16:22 -0500

    updated login, register and hooks

--- digital-booking-fe/src/Components/Login/Login.js
+++ digital-booking-fe/src/Components/Login/Login.js
@@ -22,17 +22,17 @@ const Login = () => {
             <form onSubmit={handleSubmit} noValidate>
             <label htmlFor="email">Email</label>
 
-                <input type="email" id="email" name="email" value={values.email || ''} onChange={handleChange} className={errors.email && selectedFields.includes("email") ? 'inputError' : undefined}></input>
-                {(errors.email && isSubmitting.current && (<div className='errorBox'><p className ="errorDesc">{errors.email}</p></div>)) || (errors.email && selectedFields.includes("email") && (<div className='errorBox'><p className ="errorDesc">{errors.email}</p></div>))}
+                <input type="email" id="email" name="email" value={values.email || ''} onChange={handleChange} className={errors.email && selectedFields.includes("email") ? styles.inputError : undefined}></input>
+                {(errors.email && isSubmitting.current && (<div className={styles.errorBox}><p className ={styles.errorDesc}>{errors.email}</p></div>)) || (errors.email && selectedFields.includes("email") && (<div className={styles.errorBox}><p className ={styles.errorDesc}>{errors.email}</p></div>))}
 
 
                 <label htmlFor="password">Contraseña</label>
-                <input type="password" id="password" name="password" value={values.password || ''} onChange={handleChange} className={errors.password && selectedFields.includes("password") ? 'inputError' : undefined}></input>
-                {(errors.password && isSubmitting.current && (<div className='errorBox'><p className ="errorDesc">{errors.password}</p></div>)) || (errors.password && selectedFields.includes("password") && (<div className='errorBox'><p className ="errorDesc">{errors.password}</p></div>))}
+                <input type="password" id="password" name="password" value={values.password || ''} onChange={handleChange} className={errors.password && selectedFields.includes("password") ? styles.inputError : undefined}></input>
+                {(errors.password && isSubmitting.current && (<div className={styles.errorBox}><p className ={styles.errorDesc}>{errors.password}</p></div>)) || (errors.password && selectedFields.includes("password") && (<div className={styles.errorBox}><p className ={styles.errorDesc}>{errors.password}</p></div>))}
 
 
                 <input type="submit" value="Ingresar"></input>
-                {(validCredentials===false && (<div className='errorBox'><p className ="errorDesc">Por favor, vuelva a intentarlo sus credenciales son inválidas</p></div>))}
+                {(validCredentials===false && (<div className={styles.errorBox}><p className ={styles.errorDesc}>Por favor, vuelva a intentarlo sus credenciales son inválidas</p></div>))}
 
             </form>
             <p>¿Aún no tenes cuenta? Registrate</p>
