10ae64c72dd5d41b8d2a9a5b3894321139769bac
Ametti12 <angiemt12@gmail.com>
2021-11-07 19:31:38 -0500

    Galeria con requerimientos desktop

--- digital-booking-fe/src/Components/Detalle/Responsive.js
+++ digital-booking-fe/src/Components/Detalle/Responsive.js
@@ -2,5 +2,5 @@ import React from "react";
 import Responsive from "react-responsive";
 
 
-export const Mobile = props => <Responsive {...props} maxWidth={1200} />;
+export const Mobile = props => <Responsive {...props} maxWidth={1100} />;
 export const Default = props => <Responsive {...props} minWidth={1201} />;
