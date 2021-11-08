7e8fc639ae398a291b71d5d05121d409237d05b8
Ametti12 <angiemt12@gmail.com>
2021-11-08 12:38:53 -0500

    Implementando isDayBlocked en calendario

--- digital-booking-fe/src/Components/Detalle/Fechas/Fecha.js
+++ digital-booking-fe/src/Components/Detalle/Fechas/Fecha.js
@@ -3,6 +3,7 @@ import './react_dates_overrides.css'
 import { DateRangePicker, DayPickerRangeController } from 'react-dates';
 import 'react-dates/initialize';
 import React, { Component } from "react";
+import moment from 'moment';
 import {
     HORIZONTAL_ORIENTATION,
     VERTICAL_ORIENTATION,
@@ -11,6 +12,7 @@ import { Mobile, Tablet } from "./Responsive";
 
 
 class Fecha extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
@@ -21,13 +23,27 @@ class Fecha extends Component {
 
         }
     }
+    isOutsideRange(day) {
+        return (moment().diff(day) > 0);
+      }
+      isBlocked = momentDate => {
+        const dayNumber = momentDate.format('MM/DD/YYYY');
+        return dayNumber === "11/18/2021" || dayNumber === "11/19/2021" 
+        || dayNumber === "12/25/2021" || dayNumber === "12/24/2021" || dayNumber === "12/23/2021"
+        // const dayString = momentDate.format('dd');
+        // return dayString === 'Sa' || dayString ==='Su'
+    }
+
     render() {
         return (
 
             <>
                 <Mobile>
                     <DayPickerRangeController
+
                         numberOfMonths={1}
+                        isOutsideRange={this.isOutsideRange}
+                        isDayBlocked = {this.isBlocked}
                         startDate={this.state.startDate} // momentPropTypes.momentObj or null,
                         endDate={this.state.endDate} // momentPropTypes.momentObj or null,
                         onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
@@ -39,6 +55,9 @@ class Fecha extends Component {
                 <Tablet>
                     <DayPickerRangeController
                         numberOfMonths={2}
+                        isOutsideRange={this.isOutsideRange}
+                        isDayBlocked = {this.isBlocked}
+                        enableOutsideDays={false}
                         startDate={this.state.startDate} // momentPropTypes.momentObj or null,
                         endDate={this.state.endDate} // momentPropTypes.momentObj or null,
                         onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
@@ -47,17 +66,7 @@ class Fecha extends Component {
                         // initialVisibleMonth={() => moment().add(2, "M")} // PropTypes.func or null,
                     />
 
-                    {/* <DateRangePicker
-                        numberOfMonths={2}
-                        orientation={HORIZONTAL_ORIENTATION}
-                        startDate={this.state.startDate} // momentPropTypes.momentObj or null,
-                        startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
-                        endDate={this.state.endDate} // momentPropTypes.momentObj or null,
-                        endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
-                        onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
-                        focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,   
-                        onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-                    /> */}
+                   
                 </Tablet>
 
             </>
