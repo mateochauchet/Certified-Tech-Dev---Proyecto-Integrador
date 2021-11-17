4f7ec1a9631aa85c05722c9490819a3793668c5a
Angélica Mejía <angiemt12@gmail.com>
2021-11-17 18:10:17 +0000

    Upload New File

---
+++ digital-booking-fe/src/Heading.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import './Heading.css'
+import CardsPuntaje from "../Cards_list/CardsPuntaje";
+import {Link} from 'react-router-dom'
+
+
+function Heading({titulo, categoria}) {
+    
+    return ( 
+        <>
+        <div className="heading">
+            <div className="text-heading">
+                <p>{categoria}</p>
+                <h1>{titulo}</h1> 
+            </div>
+            <div className="arrow">
+            <Link className="link-arrow" to="/home"><FontAwesomeIcon icon={faChevronLeft} /></Link>
+            </div>
+             
+        </div>
+        
+        </>
+     );
+}
+
+export default Heading;
