983eb18124cc94758b0e571374a91db7fa77bc47
sofipz <sofiaperezzavala@gmail.com>
2021-12-04 12:37:55 -0300

    no modifique nada

--- digital-booking-fe/src/Components/Cards_list/CardsImg.js
+++ digital-booking-fe/src/Components/Cards_list/CardsImg.js
@@ -4,17 +4,28 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import ContextHeart from "../Contexts/ContextHeart";
 
 function CardsImg (props) {
-    const [heartId, setHeartId] = useState(false);
+    const [heartId, setHeartId] = useState(
+       console.log( window.localStorage.getItem("heartValue"))
+    );
     //const {contextHeart, setContextHeart} = useContext(ContextHeart);
     const clickHeart = () => {
         setHeartId(!heartId)
         //setContextHeart("h")
     }
 
+    const setLocalStorage = () =>{
+        try{
+            setHeartId(!heartId)
+            window.localStorage.setItem("heartValue", heartId)
+            console.log(localStorage.getItem("heartValue"))
+        } catch(error){
+            console.log(error)
+        }
+    }
     return(
         <div className="cardsImgContainer">
             <img className="cardsImg"  src={props.house.imagenes[0].imagen} />
-            <div className= "corazon" id={heartId ? "changeColor" : null}  onClick={clickHeart}>
+            <div className= "corazon" /*id={heartId ? null : "changeColor"}  onClick={setLocalStorage}*/>
                 <FontAwesomeIcon icon={faHeart}/>
             </div>
         </div>
